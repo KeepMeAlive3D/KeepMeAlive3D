656f9c81c4b45303f422a43a8c68cb9c92ef1e0d
Matthias <matthias.klenz@outlook.com>
2024-10-09 21:15:36 +0200

    feat: add basic model to frontend and render it

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import {useState} from 'react'
 import './App.css'
+import AppCanvas from "./scene/Scene.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    //const [count, setCount] = useState(0)
+    return (
+        <div className={"main-body"}>
+            <header className={"top-app-bar"}>
+                <h3>KeepMeAlive3D</h3>
+            </header>
+            <main className={"main-content"}>
+                <nav>Nav</nav>
+                <AppCanvas></AppCanvas>
+            </main>
+            <footer className={"bottom-app-bar"}>Footer</footer>
+        </div>
+    )
 }
 
 export default App
