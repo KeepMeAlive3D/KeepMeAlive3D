9b6bb69af8c3e0a49ae8486fae78cec6a4c69e7a
matthias <matthias.klenz@outlook.com>
2024-10-18 21:29:15 +0200

    refactor: move plugin loading to Loader.kt

--- backend/api/src/main/kotlin/de/keepmealive3d/Application.kt
+++ backend/api/src/main/kotlin/de/keepmealive3d/Application.kt
@@ -1,7 +1,6 @@
 package de.keepmealive3d
 
 import de.keepmealive3d.config.Config
-import de.keepmealive3d.event.LiveDataEventHandler
 import de.keepmealive3d.plugins.*
 import de.keepmealive3d.scriptingapi.Loader
 import de.keepmealive3d.scriptingapi.PluginConfig
@@ -9,10 +8,6 @@ import de.keepmealive3d.scriptingapi.mqtt.MqttPlugin
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import kotlinx.coroutines.CoroutineStart
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import java.io.File
 
 fun main() {
@@ -27,31 +22,9 @@ fun Application.module() {
     }
 
     val loader = Loader(conf.pluginDirs.map { File(it) })
+    //current workaround: MqttPlugin stays in the :api module for better debugging experience
     loader.plugins.add(MqttPlugin() to PluginConfig("mqtt", "<none>", "1"))
-    val liveDataEventHandler = LiveDataEventHandler()
-    runBlocking {   //we want to wait until all plugins are loaded
-        loader.plugins.forEach { p ->
-            try {
-                p.first.onEnable(conf)
-            } catch (e: Exception) {
-                log.error("could not enable plugin: ${p.second.name}")
-                e.printStackTrace()
-            }
-        }
-    }
-
-    launch(Dispatchers.Default, CoroutineStart.DEFAULT) {
-        loader.plugins.forEach { p ->
-            p.first.registerKtorPlugin(this@module)
-        }
-    }
-
-
-    loader.plugins.forEach { p ->
-        launch {
-            p.first.registerLiveDataAdapter(liveDataEventHandler::receive) { false }
-        }
-    }
+    loader.loadPlugins(this, conf)
 
     configureSecurity()
     configureHTTP()
--- backend/api/src/main/kotlin/de/keepmealive3d/scriptingapi/Loader.kt
+++ backend/api/src/main/kotlin/de/keepmealive3d/scriptingapi/Loader.kt
@@ -2,6 +2,13 @@ package de.keepmealive3d.scriptingapi
 
 import com.charleskorn.kaml.PolymorphismStyle
 import com.charleskorn.kaml.Yaml
+import de.keepmealive3d.config.Config
+import de.keepmealive3d.event.LiveDataEventHandler
+import io.ktor.server.application.*
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.decodeFromString
 import java.io.File
 import java.net.URLClassLoader
@@ -27,6 +34,30 @@ class Loader(private val pluginDirectories: List<File>) {
             val classToLoad = classLoader.loadClass(conf.main)
             (classToLoad.kotlin.createInstance() as Plugin) to conf
         }.toList().toMutableList()
+    }
+
+    fun loadPlugins(application: Application, conf: Config) = with(application) {
+        val liveDataEventHandler = LiveDataEventHandler()
+        runBlocking {   //we want to wait until all plugins are loaded
+            plugins.forEach { p ->
+                try {
+                    p.first.onEnable(conf)
+                } catch (e: Exception) {
+                    log.error("could not enable plugin: ${p.second.name}")
+                    e.printStackTrace()
+                }
+            }
+        }
+        launch(Dispatchers.Default, CoroutineStart.DEFAULT) {
+            plugins.forEach { p ->
+                p.first.registerKtorPlugin(this@with)
+            }
+        }
+        plugins.forEach { p ->
+            launch {
+                p.first.registerLiveDataAdapter(liveDataEventHandler::receive) { false }
+            }
+        }
     }
 
     private fun createYamlParser(): Yaml {
