1d6a9b48736841b6d1a4c836058eab328095d4f3
matthias <matthias.klenz@outlook.com>
2024-11-07 11:25:49 +0100

    fix: update swagger dependency to be compatible with ktor 3

--- backend/api/src/main/kotlin/de/keepmealive3d/plugins/Routing.kt
+++ backend/api/src/main/kotlin/de/keepmealive3d/plugins/Routing.kt
@@ -1,7 +1,8 @@
 package de.keepmealive3d.plugins
 
 import io.github.smiley4.ktorswaggerui.SwaggerUI
-import io.ktor.http.*
+import io.github.smiley4.ktorswaggerui.routing.openApiSpec
+import io.github.smiley4.ktorswaggerui.routing.swaggerUI
 import io.ktor.resources.*
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
@@ -9,17 +10,12 @@ import io.ktor.server.resources.*
 import io.ktor.server.resources.Resources
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.ktor.server.webjars.*
 import kotlinx.serialization.Serializable
 
 fun Application.configureRouting() {
-    install(Webjars) {
-        path = "/webjars" //defaults to /webjars
-    }
     install(SwaggerUI) {
         swagger {
-            swaggerUrl = "swagger-ui"
-            forwardRoot = true
+
         }
         info {
             title = "Example API"
@@ -36,14 +32,17 @@ fun Application.configureRouting() {
         get("/hello") {
             call.respondText("Hello World!")
         }
-        get("/webjars") {
-            call.respondText("<script src='/webjars/jquery/jquery.js'></script>", ContentType.Text.Html)
-        }
         get<Articles> { article ->
             // Get all articles ...
             call.respond("List of articles sorted starting from ${article.sort}")
         }
-        staticResources("/", "static")
+        staticResources("/static", "static")
+        route("api.json") {
+            openApiSpec()
+        }
+        route("swagger") {
+            swaggerUI("/api.json")
+        }
     }
 }
 
