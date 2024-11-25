d665799f254661c0069d64925cb0b2e3ef4de8f7
Clemens <mail@clemens.jann.xyz>
2024-11-25 10:40:59 +0100

    feat: pull asynchronicity one layer up

--- backend/api/src/main/kotlin/de/keepmealive3d/event/LiveDataEventHandler.kt
+++ backend/api/src/main/kotlin/de/keepmealive3d/event/LiveDataEventHandler.kt
@@ -1,34 +1,22 @@
 package de.keepmealive3d.event
 
 import de.keepmealive3d.adapters.influx.KmaInfluxDatabase
-import kotlinx.coroutines.channels.consumeEach
 import kotlinx.coroutines.flow.consumeAsFlow
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import org.koin.core.component.KoinComponent
 
 class LiveDataEventHandler : EventHandler, KoinComponent {
     private val database = KmaInfluxDatabase()
 
 
-    fun receive(topic: String, payload: String) {
-
-        runBlocking {
-            launch {
-                try {
-                    // TODO: write actual data
-                    database.write("mem", "test", 42.0)
-
-                    database.readAllMeasurements("mem").consumeAsFlow().collect { println("${it.value}") }
-
-                } catch (exception: Exception) {
-                    println(exception)
-                }
-
-            }
+    suspend fun receive(topic: String, payload: String) = withContext(Dispatchers.IO) {
+        try {
+            // TODO: write actual data
+            database.write("mem", "test", 42.0)
+            database.readAllMeasurements("mem").consumeAsFlow().collect { println("${it.value}") }
+        } catch (exception: Exception) {
+            println(exception)
         }
-
-
         println("Received on topic: $topic -> $payload")
     }
 }
