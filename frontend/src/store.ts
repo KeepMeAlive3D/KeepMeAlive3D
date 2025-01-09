37bf75bf4c7b2362569500a8ea903f4653ff956d
Clemens <mail@clemens.jann.xyz>
2025-01-09 14:34:19 +0100

    fear: sync store across tabs

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,19 +1,20 @@
-import {configureStore} from '@reduxjs/toolkit'
-import modelPartSlice from "@/slices/ModelPartSlice.ts";
-import settingsSlice from "@/slices/SettingsSlice.ts";
+import {combineReducers, configureStore} from '@reduxjs/toolkit'
+import modelPartSlice from "@/slices/ModelPartSlice.ts"
+import settingsSlice from "@/slices/SettingsSlice.ts"
+import {createStateSyncMiddleware, initStateWithPrevTab, withReduxStateSync} from "redux-state-sync"
 
 const store = configureStore({
-    reducer: {
+    reducer: withReduxStateSync(combineReducers({
         modelParts: modelPartSlice,
         settings: settingsSlice,
-    },
-    middleware: getDefaultMiddleware =>
-        getDefaultMiddleware({
-            serializableCheck: false,
-        }),
+    })),
+
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(createStateSyncMiddleware()),
 })
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 
+initStateWithPrevTab(store);
 export default store;
