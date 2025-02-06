24677d2d65b88eb1823979eec97a0f3a84bc5142
Matthias <matthias.klenz@outlook.com>
2025-02-06 19:02:43 +0100

    upgrade tailwind to 3.0.1

--- frontend/src/scene/Edit.tsx
+++ frontend/src/scene/Edit.tsx
@@ -7,7 +7,7 @@ function Edit() {
 
   return (
     <div className="edit-content flex flex-col h-auto">
-      <div className="canvas-content flex-grow">
+      <div className="canvas-content grow">
         {url && (
           <Suspense>
             <DynamicModel objectUrl={url} />
