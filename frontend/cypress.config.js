7b199fc7c16f81893f7a09bb78834cf3abe5211d
Clemens <mail@clemens.jann.xyz>
2025-02-07 13:58:31 +0100

    test: add test to check store update

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -4,14 +4,15 @@ import axios from "axios";
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      // Note: this is not run if 'npx cypress open' mode is used.
       on("before:run", (details) => {
         const options = {
           method: "POST",
           url: "http://localhost:8080/api/register/basic",
           headers: { "Content-Type": "application/json" },
-          data: { username: "admin", password: "123" },
+          data: { username: "tester", password: "123" },
         };
-
+        console.log("Running REGISTER");
         axios
           .request(options)
           .then(function(response) {
