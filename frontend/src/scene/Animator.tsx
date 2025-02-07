16228cbd0de01b15cabf0e1567436b2910935b70
Clemens <mail@clemens.jann.xyz>
2025-02-07 11:09:15 +0100

    fix: readjust coordinates again

--- frontend/src/scene/Animator.tsx
+++ frontend/src/scene/Animator.tsx
@@ -12,8 +12,8 @@ function Animator() {
     const selectedObject = state.scene.getObjectByName("9V_querausleger_sauger_id2046_step");
 
     if (selectedObject) {
-      console.debug("Moving object from " + selectedObject.position.toArray().join(";") + " to " + msg.message.position.x + ";" + msg.message.position.y + ";" + msg.message.position.z);
-      selectedObject.position.set(selectedObject.position.x, selectedObject.position.y, selectedObject.position.z + 0.01);
+      console.debug("Moving object from " + selectedObject.position.x + ";" + selectedObject.position.y + ";" + selectedObject.position.z + " to " + msg.message.position.x + ";" + msg.message.position.y + ";" + msg.message.position.z);
+      selectedObject.position.set(msg.message.position.x, msg.message.position.y, msg.message.position.z);
     } else {
       console.warn("Received position event for an unknown object " + msg.message.topic);
     }
