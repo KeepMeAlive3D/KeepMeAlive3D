b97c46d5ed30947142bd8607695c2d4921f1a472
Matthias <matthias.klenz@outlook.com>
2025-04-09 10:37:35 +0200

    add date-time picker

--- frontend/src/scene/Help.tsx
+++ frontend/src/scene/Help.tsx
@@ -2,8 +2,8 @@ export default function Help() {
   return (
     <div className="flex flex-row items-center justify-center">
       <div className="flex h-[150px] w-[300px] items-center justify-center rounded-md border border-dashed text-sm">
-            Open or upload a Model to display.
+        Open or upload a Model to display.
       </div>
     </div>
-  )
+  );
 }
