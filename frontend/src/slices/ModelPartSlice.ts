35467bbe652f879b83e192f03471c74b7250bc68
Clemens <mail@clemens.jann.xyz>
2025-04-10 11:53:19 +0200

    refactor: reformat file

--- frontend/src/slices/ModelPartSlice.ts
+++ frontend/src/slices/ModelPartSlice.ts
@@ -31,7 +31,7 @@ export const modelPartSlice = createSlice({
     },
     toggleIsSelected: (state, action: PayloadAction<ComponentInformation>) => {
       const index = state.partIds.findIndex(
-        (part) => part.id === action.payload.id
+        (part) => part.id === action.payload.id,
       );
       if (index !== -1) {
         state.partIds[index].isSelected = !state.partIds[index].isSelected;
